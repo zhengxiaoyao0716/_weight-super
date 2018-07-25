671d32233d734bc2aa78939de5d9330a6fd5f31b
zhengxiaoyao0716 <1499383852@qq.com>
2018-07-25 11:34:24 +0800

    加一个建立Anaconda2软连接的脚本

---
+++ install.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:setdir
+set /p dir=Anaconda2安装位置（D:\Path\To\Anaconda2）：
+if exist %dir%\Scripts\anaconda.exe (
+    goto mklink
+) else (
+    echo 无效的路径
+    pause
+    goto setdir
+)
+
+:mklink
+mklink /D Anaconda2 %dir%
+pause
